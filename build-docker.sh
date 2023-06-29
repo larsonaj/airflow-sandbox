56aa803f54b5418c6bd3433801e515f9723e6e6d
Jason Wang <jwang@sl-015222222357.localdomain>
2023-06-29 16:59:33 -0400

    updating docker build commands

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,2 @@
-docker build airflow_containers -t captech-demo-airflow:0.0.1
-docker build dags/docker/python -t captech-airflow-sandbox-python:0.0.1
+docker build airflow_containers -t captech-demo-airflow:0.0.1 --no-cache
+docker build dags/docker/python -t captech-airflow-sandbox-python:0.0.1 --no-cache
